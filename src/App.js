919a3c3a0c1dc80838e339b521d7145efb524cf5
CYGNET\tejas.ahire <tejas.ahire@cygnet.one>
2025-02-08 17:37:29 +0530

    added homepage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
               <Router>
                 <Routes>
                     <Route path='/' 
-                        element={<Home />} />
+                        element={<Home_page />} />
                     <Route path='/create' 
                         element={<Create />} />
                     <Route path='/edit' 
